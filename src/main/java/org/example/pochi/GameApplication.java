dfc0d383fe0fb1b9255be256b77e0d8afcde58ee
Nowiisss <1702482@uab.cat>
2024-12-01 11:10:07 +0100

    Icon canviat

--- src/main/java/org/example/pochi/GameApplication.java
+++ src/main/java/org/example/pochi/GameApplication.java
@@ -4,17 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.image.Image;
 import java.io.IOException;
 
 public class GameApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
+    // Cargar la vista principal
     FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+
+    // Configurar título e icono de la aplicación
     stage.setTitle("Pochify");
+    stage.getIcons().add(new Image(GameApplication.class.getResourceAsStream("/org/example/pochi/icon.png"))); // Ruta del icono
     stage.setScene(scene);
 
+    // Mostrar la ventana
     stage.show();
   }
 
